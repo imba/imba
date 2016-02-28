0bd09e1d3a46bbfd737bc6397fa91986f84f70bb
somebee <sindre@identu.no>
2016-02-28 15:35:38 +0100

    Simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,83 +2,77 @@ var path = require('path');
 var webpack = require('webpack');
 
 var prodDefines = new webpack.DefinePlugin({
-	"Imba.SERVER": false,
-	"Imba.DEBUG": false,
 	"Imba.CLIENT": true
 })
 
 var prod = [
 	new webpack.DefinePlugin({
-		"Imba.SERVER": false,
-		"Imba.DEBUG": false,
 		"Imba.CLIENT": true
 	}),
-	new webpack.optimize.UglifyJsPlugin({minimize: true})
+	new webpack.optimize.UglifyJsPlugin({
+		minimize: true,
+		compress: { warnings: false }
+	})
 ]
 
 var dev = [
 	new webpack.DefinePlugin({
-		"Imba.SERVER": false,
 		"Imba.DEBUG": true,
 		"Imba.CLIENT": true
 	})
 ]
 
+var loaderPath = path.join(__dirname, "./loader");
+var loaders = [{ "test": /\.imba$/, "loader": loaderPath}];
+
 var resolveLoader = {
-	// root: path.join(__dirname, "node_modules")
-	alias: {
-		"imba-loader": path.join(__dirname, "./loader"),
-		"path": "path-browserify"
-	}
+	alias: { "path": "path-browserify" }
 }
 
-module.exports = [{
-	module: {loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]},
-	resolveLoader: resolveLoader,
-	resolve: {extensions: ['', '.imba', '.js']},
+function pkg(options){
+	var pkg = {
+		module: {loaders: loaders},
+		resolveLoader: resolveLoader,
+		resolve: {extensions: ['', '.imba', '.js']},
+		entry: "./src/imba/index.imba",
+		output: { filename: "./dist/imba.js" }
+	}
+	Object.keys(options).map(function(key){
+		pkg[key] = options[key];
+	})
+
+	return pkg;
+}
+
+module.exports = [pkg({
 	entry: "./src/imba/index.imba",
 	output: { filename: "./dist/imba.dev.js" },
 	plugins: dev
-},{
-	module: {loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]},
-	resolveLoader: resolveLoader,
-	resolve: {extensions: ['', '.imba','.js']},
+}),pkg({
 	entry: "./src/imba/index.imba",
 	output: { filename: "./dist/imba.js" },
 	plugins: [prodDefines]
-},{
-	module: {loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]},
-	resolveLoader: resolveLoader,
-	resolve: {extensions: ['', '.imba','.js']},
+}),pkg({
 	entry: "./src/imba/index.imba",
 	output: { filename: "./dist/imba.min.js" },
 	plugins: prod
-},{
-	module: {loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]},
-	resolveLoader: resolveLoader,
-	resolve: {extensions: ['', '.js', '.imba']},
+}),pkg({
 	entry: "./src/compiler/compiler.imba",
 	output: { filename: "./dist/imbac.dev.js", library: "Imbac" },
 	target: 'web',
 	node: {fs: "empty"},
 	plugins: dev
-},{
-	module: {loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]},
-	resolveLoader: resolveLoader,
-	resolve: {extensions: ['', '.js', '.imba']},
+}),pkg({
 	entry: "./src/compiler/compiler.imba",
 	output: { filename: "./dist/imbac.js", library: "Imbac" },
 	target: 'web',
 	node: {fs: "empty"},
 	plugins: prod
-},{
-	module: {loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]},
-	resolveLoader: resolveLoader,
-	resolve: {extensions: ['', '.js', '.imba']},
+}),pkg({
 	entry: "./test/index.imba",
 	output: { filename: "./test/client.js"},
 	target: 'web',
 	node: {fs: "empty"},
 	plugins: dev
-}]
+})]
 
