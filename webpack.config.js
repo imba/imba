b08026c542d263009d86d58716f6b76edb9ac036
Sindre Aarsaether <sindre@identu.no>
2019-11-21 14:38:24 +1000

    Refactoring build process++

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,15 @@
 var path = require('path');
-var webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
 var package = require('./package.json');
-var loader = path.join(__dirname, "./loader");
-
-var minify = new UglifyJsPlugin({
-	uglifyOptions: {
-		ecma: 6,
-		minimize: true,
-		compress: { warnings: false },
-		output: {
-			semicolons: false,
-			indent_level: 0
-		}
-	}
-	
-});
+var loader = path.join(__dirname, "loader");
 
 module.exports = [{
 	module: {
-		rules: [{test: /\.imba$/, loader: loader, options: {es5: true}}]
+		rules: [{test: /\.imba$/, use: loader}]
 	},
 	resolve: {extensions: ['*', '.imba', '.js']},
-	entry: "./src/imba/index.imba",
+	entry: "./index.imba",
 	output: {
-		filename: "./imba.js",
+		filename: "./dist/imba.development.js",
 		library: {
 			root: "Imba",
 			amd: "imba",
@@ -35,33 +19,16 @@ module.exports = [{
 		path: path.resolve(__dirname),
 		libraryTarget: "umd"
 	},
+	node: {fs: false, process: false, global: false}
+},{
+	resolve: {extensions: ['.js']},
+	entry: "./lib/compiler/compiler.js",
+	output: {
+		filename: "./dist/imbac.js",
+		library: "imbac",
+		libraryTarget: "umd",
+		globalObject: 'typeof self !== \'undefined\' ? self : this',
+		path: path.resolve(__dirname)
+	},
 	node: {fs: false, process: false, global: false},
-	plugins: [minify]
-//},{
-//	module: {
-//		rules: [{test: /\.imba$/, loader: 'imba/imba', options: {es5: true}}]
-//	},
-//	resolve: {extensions: ['*', '.imba', '.js']},
-//	entry: "./src/compiler/compiler.imba",
-//	output: {
-//		filename: "./imbac.js",
-//		library: "imbac",
-//		libraryTarget: "umd",
-//		globalObject: 'typeof self !== \'undefined\' ? self : this',
-//		path: path.resolve(__dirname)
-//	},
-//	node: {fs: false, process: false, global: false},
-//	plugins: [minify]
-// },{
-// 	module: {
-// 		rules: [{test: /\.imba$/, loader: loader}]
-// 	},
-// 	resolve: {extensions: ['*', '.imba', '.js']},
-// 	entry: "./test/index.imba",
-// 	output: {
-// 		filename: "./test/client.js",
-// 		globalObject: 'typeof self !== \'undefined\' ? self : this',
-// 		path: path.resolve(__dirname)
-// 	},
-// 	node: {fs: false, process: false, global: false}
 }]
