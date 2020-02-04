a5179a195119727ecc2f278a89c276e9e8417491
Sindre Aarsaether <sindre@identu.no>
2020-02-04 05:29:45 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -6,12 +6,19 @@ interface Element {
     unschedule(): this;
 }
 
+interface ImbaElement implements Element {
+}
+
 interface Imba {
     setInterval(handler: TimerHandler, timeout?: number, ...arguments: any[]): number;
     setTimeout(handler: TimerHandler, timeout?: number, ...arguments: any[]): number;
     clearInterval(handle?: number): void;
     clearTimeout(handle?: number): void;
     commit(): Promise<this>;
+
+    createIndexedFragment(...arguments: any[]): DocumentFragment;
+    createKeyedFragment(...arguments: any[]): DocumentFragment;
+    createLiveFragment(...arguments: any[]): DocumentFragment;
 }
 
 declare const imba: Imba
