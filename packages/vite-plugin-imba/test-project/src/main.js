2f34d1c8cab143d442b3dfbbd23ac6e02ddf1e12
Abdellah Alaoui Solaimani <als.abdellah@gmail.com>
2022-09-01 14:54:00 +0300

    cleanup (#705)
    * cleanup
    
    * revert adding a vite template

--- packages/vite-plugin-imba/test-project/src/main.js
+++ packages/vite-plugin-imba/test-project/src/main.js
@@ -1,10 +1,2 @@
 import './styles.imba'
-// import App from './App.svelte'
-
 import './main.imba'
-
-// const app = new App({
-//   target: document.getElementById('app')
-// })
-
-// export default app
