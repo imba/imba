2865b7b572e0d30c5304cbfe383160494dbed20b
familyfriendlymikey <ozmichaelt@icloud.com>
2023-07-06 10:30:43 +0200

    vitest ok and nok with multiple arguments translate to eq and neq

--- packages/imba/bin/test-setup.all.mjs
+++ packages/imba/bin/test-setup.all.mjs
@@ -2,7 +2,18 @@ import {vi} from "vitest"
 import assert from "assert"
 import 'imba/std'
 
-vi.stubGlobal('eq', assert.deepStrictEqual)
-vi.stubGlobal('neq', assert.notDeepStrictEqual)
-vi.stubGlobal('ok', assert.ok)
-vi.stubGlobal('nok', (b) => assert.ok(!b))
+vi.stubGlobal('ok',function(_0) {
+	if (arguments.length <= 1) {
+		return assert.ok(_0);
+	} else {
+		return assert.deepStrictEqual(...arguments);
+	};
+});
+
+vi.stubGlobal('nok',function(_0) {
+	if (arguments.length <= 1) {
+		return assert.ok(!(_0));
+	} else {
+		return assert.notDeepStrictEqual(...arguments);
+	};
+});
--- packages/imba/bin/test-setup.browser.mjs
+++ packages/imba/bin/test-setup.browser.mjs
@@ -3,10 +3,21 @@ import {vi} from "vitest"
 import * as domHelpers from '@testing-library/dom'
 import 'imba/std'
 
-vi.stubGlobal('eq', assert.deepStrictEqual)
-vi.stubGlobal('neq', assert.notDeepStrictEqual)
-vi.stubGlobal('ok', assert.ok)
-vi.stubGlobal('nok', (b) => assert.ok(!b))
+vi.stubGlobal('ok',function(_0) {
+	if (arguments.length <= 1) {
+		return assert.ok(_0);
+	} else {
+		return assert.deepStrictEqual(...arguments);
+	};
+});
+
+vi.stubGlobal('nok',function(_0) {
+	if (arguments.length <= 1) {
+		return assert.ok(!(_0));
+	} else {
+		return assert.notDeepStrictEqual(...arguments);
+	};
+});
 
 class MockPointerEvent{}
 vi.stubGlobal("PointerEvent", MockPointerEvent)
