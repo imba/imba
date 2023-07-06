9f9e89b91148fa3bc97533c3353bda58fdc233e0
familyfriendlymikey <ozmichaelt@icloud.com>
2023-07-06 20:08:04 +0200

    vitest add assert.rejects

--- packages/imba/bin/test-setup.all.mjs
+++ packages/imba/bin/test-setup.all.mjs
@@ -2,6 +2,8 @@ import {vi} from "vitest"
 import assert from "assert"
 import 'imba/std'
 
+vi.stubGlobal('rejects', assert.rejects)
+
 vi.stubGlobal('ok',function(_0) {
 	if (arguments.length <= 1) {
 		return assert.ok(_0);
--- packages/imba/bin/test-setup.browser.mjs
+++ packages/imba/bin/test-setup.browser.mjs
@@ -3,6 +3,8 @@ import {vi} from "vitest"
 import * as domHelpers from '@testing-library/dom'
 import 'imba/std'
 
+vi.stubGlobal('rejects', assert.rejects)
+
 vi.stubGlobal('ok',function(_0) {
 	if (arguments.length <= 1) {
 		return assert.ok(_0);
