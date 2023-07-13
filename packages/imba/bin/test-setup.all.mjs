ba965c0e846d4e00c36a3578f5c2b29cfe067754
abdellah <als.abdellah@gmail.com>
2023-07-13 17:42:56 +0300

    move everything inside try catch for stacktrace

--- packages/imba/bin/test-setup.all.mjs
+++ packages/imba/bin/test-setup.all.mjs
@@ -1,31 +1,30 @@
-import {vi} from "vitest"
+import { vi } from "vitest"
 import assert from "assert"
 import 'imba/std'
 
 vi.stubGlobal('rejects', assert.rejects)
 
-const ok = (nook = false)=> (...args) =>{
-
-	if (args.length <= 1) {
-		if(nook){
-			return expect(args[0]).not.toBeTruthy()
-		}else{
-			return expect(args[0]).toBeTruthy()
-		}
-	} else {
-		const [a, b, msg] = args
-		try{
-			if(nook){
+const ok = (nook = false) => (...args) => {
+	try {
+		if (args.length <= 1) {
+			if (nook) {
+				return expect(args[0]).not.toBeTruthy()
+			} else {
+				return expect(args[0]).toBeTruthy()
+			}
+		} else {
+			const [a, b] = args
+			if (nook) {
 				expect(a).not.toStrictEqual(b)
-			}else{
+			} else {
 				expect(a).toStrictEqual(b)
 			}
-		} catch(error){
-			if(msg) console.error(msg)
-			throw error
 		}
-	};
+	} catch (error) {
+		if (args[2]) console.error(args[2])
+		throw error
+	}
 }
 vi.stubGlobal('ok', ok());
 
-vi.stubGlobal('nok',ok(true));
+vi.stubGlobal('nok', ok(true));
